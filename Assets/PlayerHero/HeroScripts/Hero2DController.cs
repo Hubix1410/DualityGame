2d8e1e03c96c544b0f480a0c908a4d5deb507d85
Hubert Radzewicz <Hubert.rdza@gmail.com>
2022-01-28 22:09:16 +0100

    added rotation

--- Assets/PlayerHero/HeroScripts/Hero2DController.cs
+++ Assets/PlayerHero/HeroScripts/Hero2DController.cs
@@ -7,6 +7,9 @@ public class Hero2DController : MonoBehaviour
     public float MovementSpeed = 5f;
     public float JumpForce = 5f;
 
+    public GameObject HeroObject;
+
+    private int RotationDirection;
     private Rigidbody2D _rigidbody;
 
     void Start()
@@ -23,5 +26,27 @@ public class Hero2DController : MonoBehaviour
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
+
+        IsGrounded();
+        RotationCheck();
+    }
+
+    void RotationCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && RotationDirection == 0)
+        {
+            HeroObject.transform.Rotate(0, 180, 0);
+            RotationDirection = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && RotationDirection == 1)
+        {
+            HeroObject.transform.Rotate(0, 180, 0);
+            RotationDirection = 0;
+        }
+    }
+
+    private bool IsGrounded() {
+        return true;
     }
 }
